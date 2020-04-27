434d9349ab669ec419b2ffaef3161f75c0e1d0f4
Guillaume Raille <guillaume.raille@iod.ai>
2020-04-27 20:00:29 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+setup(
+    name='saleor-gql-loader',
+    packages=['saleor-gql-loader'],
+    version='0.0.1',
+    license='MIT',
+    description='A simple gql loader class to create some entities in Saleor',
+    author='Guillaume Raille',
+    author_email='guillaume.raille@gmail.com',
+    url='https://github.com/grll/saleor-gql-loader',
+    download_url='https://github.com/grll/saleor-gql-loader/archive/v_0.0.1.tar.gz',
+    keywords=['graphql', 'saleor', 'loader'],
+    install_requires=[
+        'requests',
+        'beautifulsoup4',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
